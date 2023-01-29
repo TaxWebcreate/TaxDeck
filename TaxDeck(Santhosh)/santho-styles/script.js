3926ef3696fc4a73e7da95ad68c63cbff6327798
Santho257 <123233645+Santho257@users.noreply.github.com>
2023-01-29 16:21:02 +0530

    Santhosh-Pages

---
+++ TaxDeck(Santhosh)/santho-styles/script.js
@@ -0,0 +1,29 @@
+function expand(clicked_id){
+	var allMinus=document.getElementsByClassName("fa-minus");
+	for(var i=0;i<allMinus.length;i++)
+		allMinus[i].style.visibility = "hidden";
+	var allPlus=document.getElementsByClassName("fa-plus");
+	for(var i=0;i<allPlus.length;i++)
+		allPlus[i].style.visibility = "visible";
+	var ans;
+	var anss = document.getElementsByClassName("answer");
+	var qstn = document.getElementsByClassName("question");
+	for(var i=0;i<qstn.length;i++){
+		if(clicked_id == qstn[i].id){
+			ans=document.getElementById(anss[i].id);
+		}else{
+			anss[i].style.display= "none";
+		}
+	}
+	var sym=document.getElementById(clicked_id).getElementsByClassName("fa");
+	for(var i=0;i<sym.length;i++)
+		sym[i].style.visibility="hidden";
+	if(ans.style.display == "none"){
+		sym[1].style.visibility="visible"; 
+		ans.style.display="block";
+	}
+	else{
+		sym[0].style.visibility="visible";
+		ans.style.display="none";
+	}; 
+}
